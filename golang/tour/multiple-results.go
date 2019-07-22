1ec86870b96bd7c6bf5c67ac1b0c53794aa9bbe9
stephenhuh <stephenhuh@gmail.com>
2019-07-22 12:07:23 -0400

    golang tour stuff

--- golang/tour/multiple-results.go
+++ golang/tour/multiple-results.go
@@ -2,4 +2,14 @@ package main
 
 import "fmt"
 
-func swap(x, y )
+func swap (x, y string)(string, string) {
+	return y, x
+}
+
+
+func main() {
+	a, b := swap("hello", "world")
+	fmt.Println(a, b)
+}
+
+
