ee8ef764a4f427988dc8f3e03e8e268e18d7dcf4
stevie huh <stevie.huh@bcgdv.com>
2019-08-28 20:35:49 -0400

    swift init

---
+++ swift/book.swift
@@ -0,0 +1,7 @@
+print("Hello world")
+var myVar = 52
+print(myVar)
+myVar = 53
+print(myVar)
+let myConst = 42
+print(myConst)
---
+++ swift/th.swift
@@ -0,0 +1,17 @@
+class someClass {
+  static func someMethod() {
+  
+  }
+}
+
+// Type properties
+struct Point {
+  let x: Int
+  let y: Int
+}
+
+
+let arr = [0,1,2,3,4]
+let randIndex = Int.random(in: 0...arr.count-1)
+print(arr[randIndex])
+
