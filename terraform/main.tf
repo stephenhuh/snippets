75403a1060f398c3f5da4b5f7777c4f6d6d1d59e
Stephen Huh <stephenhuh@gmail.com>
2022-02-07 18:50:39 -0500

    terraform tutorial

---
+++ terraform/main.tf
@@ -0,0 +1,21 @@
+terraform {
+  required_providers {
+    google = {
+      source = "hashicorp/google"
+      version = "3.5.0"
+    }
+  }
+}
+
+provider "google" {
+  credentials = file("key.json")
+
+  project = "mimetic-science-330314"
+  region  = "us-central1"
+  zone    = "us-central1-c"
+}
+
+resource "google_compute_network" "vpc_network" {
+  name = "terraform-network"
+}
+
