9e3c2e5aee6eae8317aaa16d6b63f992c837b5bf
Stephen Huh <stephenhuh@gmail.com>
2022-02-07 19:22:13 -0500

    add variables.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -8,11 +8,11 @@ terraform {
 }
 
 provider "google" {
-  credentials = file("key.json")
+  credentials = file(var.credentials_file)
 
-  project = "mimetic-science-330314"
-  region  = "us-central1"
-  zone    = "us-central1-c"
+  project = "var.project"
+  region  = "var.region"
+  zone    = "var.zone"
 }
 
 resource "google_compute_network" "vpc_network" {
---
+++ terraform/variables.tf
@@ -0,0 +1,12 @@
+variable "project" { }
+
+variable "credentials_file" { }
+
+variable "region" {
+  default = "us-central1"
+}
+
+variable "zone" {
+  default = "us-central1-c"
+}
+
