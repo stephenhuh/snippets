8a0cddc8415635e10515c24c47873d89ff54bb68
Stephen Huh <stephenhuh@gmail.com>
2022-02-07 19:15:24 -0500

    in place update

--- terraform/main.tf
+++ terraform/main.tf
@@ -19,3 +19,21 @@ resource "google_compute_network" "vpc_network" {
   name = "terraform-network"
 }
 
+resource "google_compute_instance" "vm_instance" {
+  name         = "terraform-instance"
+  machine_type = "f1-micro"
+  tags         = ["web", "dev"]
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-9"
+    }
+  }
+
+  network_interface {
+    network = google_compute_network.vpc_network.name
+    access_config {
+    }
+  }
+}
+
